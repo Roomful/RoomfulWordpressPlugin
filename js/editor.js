99e32ef34c9b1ae6fde0297cbac62c403e76bfc6
Dimitriy Kalugin <devinterx@gmail.com>
2018-09-22 01:04:44 +0300

    + add popup

---
+++ js/editor.js
@@ -0,0 +1,281 @@
+﻿function RoomfulEditor() {
+    this.placeholder = 'https://my.roomful.net/#/room/55mx098565068t';
+
+    this.type = 'iframe';
+    this.host = 'my.roomful.net';
+    this.room = '55mx098565068t';
+    this.width = 960;
+    this.height = 620;
+    this.ytVideo = '';
+    this.embedCode = '';
+    this.shortCode = '';
+    this.autoPlay = false;
+    this.needAuth = false;
+    this.autoChat = false;
+
+    this.addCode = function (type) {
+        if (type === undefined) type = 'embedLink';
+
+        try {
+            if ((type === 'embedLink' && this.embedCode !== '') || (type === 'shortCode' && this.shortCode !== '')) {
+                var code = type === 'embedLink' ? this.embedCode : this.shortCode;
+
+                if (code.indexOf('[') !== -1) code = '<p>' + code + '</p>';
+
+                if (window.tinyMCE !== null && window.tinyMCE.activeEditor !== null && !window.tinyMCE.activeEditor.isHidden()) {
+                    if (typeof window.tinyMCE.execInstanceCommand !== 'undefined') {
+                        window.tinyMCE.execInstanceCommand(window.tinyMCE.activeEditor.id, 'mceInsertContent', 0, code);
+                    }
+                    else {
+                        send_to_editor(code);
+                    }
+                }
+                else {
+                    code = code.replace('<p>', '\n').replace('</p>', '\n');
+                    if (typeof QTags.insertContent === 'function') {
+                        QTags.insertContent(code);
+                    }
+                    else {
+                        send_to_editor(code);
+                    }
+                }
+                tb_remove();
+            }
+        }
+        catch (error) {
+
+        }
+    }
+}
+
+RoomfulEditor.prototype.generateFrameCode = function () {
+    var query = '';
+    if (this.host.indexOf('.roomful.net') === -1) {
+        this.embedCode = this.shortCode = '';
+        return;
+    }
+
+    var mHost = this.host.split('.')[0];
+
+    if (this.autoPlay) query += ((query === '') ? '?' : '&') + 'autoplay=1';
+    if (this.needAuth) query += ((query === '') ? '?' : '&') + 'auth=1';
+    if (this.ytVideo !== '') query += ((query === '') ? '?' : '&') + 'v=' + this.ytVideo;
+    if (this.host !== 'my.roomful.net') query += ((query === '') ? '?' : '&') + 'light=1';
+
+    query += ((query === '') ? '?' : '&') + 'w=' + this.width;
+    query += ((query === '') ? '?' : '&') + 'h=' + this.height;
+
+    this.embedCode = 'https://' + this.host + '/' + (query !== '' ? query + '/' : '')
+        + '#/room/' + this.room + (this.autoChat ? '/chat' : '');
+
+    this.shortCode = '[roomful'
+        + (this.type !== 'iframe' ? ' t="' + this.type + '"' : '')
+        + (mHost !== 'my' ? ' host="' + mHost + '"' : '')
+        + (' room="' + this.room + '"')
+        + (this.autoPlay ? ' autoPlay="true"' : '')
+        + (mHost !== 'my' ? ' isLight="false"' : '')
+        + (this.needAuth ? ' needAuth="true"' : '')
+        + (this.autoChat ? ' autoChat="true"' : '')
+        + (this.ytVideo ? ' ytVideo="' + this.ytVideo + '"' : '')
+        + (' width="' + this.width + '"')
+        + (' height="' + this.height + '"')
+        + ']';
+};
+
+RoomfulEditor.prototype.update = function () {
+    this.code = this.generateFrameCode();
+
+    jQuery('#roomful-embed-code').value = this.embedCode;
+    jQuery('#roomful-short-code').value = this.embedCode;
+};
+
+RoomfulEditor.OnOpenModalWindow = function () {
+    RoomfulEditor.resizeModalAsync();
+
+    jQuery('#roomful-width').value = this.width;
+    jQuery('#roomful-height').value = this.height;
+
+    jQuery('#roomful-autoplay-switch').checked = this.autoPlay;
+    jQuery('#roomful-auth-switch').checked = this.needAuth;
+    jQuery('#roomful-chat-switch').checked = this.autoChat;
+
+    this.update();
+};
+
+RoomfulEditor.resizeModal = function () {
+    var $e = jQuery(document).find('#TB_window');
+
+    if ($e.find('#roomful-editor-popup')) {
+        $e.width(780).height(470).css('margin-left', -780 / 2).css('margin-top', 470 / 2);
+    }
+};
+
+RoomfulEditor.resizeModalAsync = function () {
+    setTimeout(this.resizeModal, 20);
+};
+
+jQuery(document).on('ready', function () {
+    if (window.location.toString().indexOf('https://') === 0) {
+        window._EPYTA_.wpajaxurl = window._EPYTA_.wpajaxurl.replace('http://', 'https://');
+    }
+
+    var roomful = new RoomfulEditor();
+
+    jQuery(window).on('resize', RoomfulEditor.resizeModalAsync);
+    jQuery(document.body).on('click', '.roomful-wordpress-button', RoomfulEditor.OnOpenModalWindow.bind(roomful));
+
+    jQuery(document.body).on('click', '#roomful-add-short-code', function () {
+        roomful.addCode('shortCode');
+    });
+    jQuery(document.body).on('click', '#roomful-add-embed-link', function () {
+        roomful.addCode('embedLink');
+    });
+
+    jQuery(document.body).on('click', '#roomful-width', function (event) {
+        event.preventDefault();
+        jQuery('#roomful-width').select();
+    }.bind(roomful));
+
+    jQuery(document.body).on('click', '#roomful-height', function (event) {
+        event.preventDefault();
+        jQuery('#roomful-height').select();
+    }.bind(roomful));
+
+    jQuery(document.body).on('change', '#roomful-width', function (event) {
+        event.preventDefault();
+        this.width = jQuery('#roomful-width').value;
+        this.update();
+    }.bind(roomful));
+
+    jQuery(document.body).on('change', '#roomful-height', function (event) {
+        event.preventDefault();
+        this.height = jQuery('#roomful-height').value;
+        this.update();
+    }.bind(roomful));
+
+    jQuery(document.body).on('focus', '#roomful-height', function (event) {
+        event.preventDefault();
+        jQuery('#roomful-height').attr('placeholder', '');
+    }.bind(roomful));
+
+    jQuery(document.body).on('blur', '#roomful-height', function (event) {
+        event.preventDefault();
+        jQuery('#roomful-height').attr('placeholder', '620');
+    }.bind(roomful));
+
+    jQuery(document.body).on('focus', '#roomful-width', function (event) {
+        event.preventDefault();
+        jQuery('#roomful-width').attr('placeholder', '');
+    }.bind(roomful));
+
+    jQuery(document.body).on('blur', '#roomful-width', function (event) {
+        event.preventDefault();
+        jQuery('#roomful-width').attr('placeholder', '960');
+    }.bind(roomful));
+
+    jQuery(document.body).on('change', '#roomful-autoplay-switch', function (event) {
+        event.preventDefault();
+        this.autoPlay = jQuery('#roomful-autoplay-switch').checked;
+        this.update();
+    }.bind(roomful));
+
+    jQuery(document.body).on('change', '#roomful-auth-switch', function (event) {
+        event.preventDefault();
+
+        this.needAuth = jQuery('#roomful-auth-switch').checked;
+
+        if (this.needAuth === false && this.autoChat === true) {
+            jQuery('#roomful-auth-switch').checked = true;
+        } else {
+            this.update();
+        }
+
+    }.bind(roomful));
+
+    jQuery(document.body).on('change', '#roomful-chat-switch', function (event) {
+        event.preventDefault();
+        this.autoChat = jQuery('#roomful-chat-switch').checked;
+
+        if (this.autoChat === true && this.needAuth === false) {
+            this.needAuth = true;
+            jQuery('#roomful-auth-switch').checked = true;
+        }
+
+        this.update();
+
+    }.bind(roomful));
+
+    jQuery(document.body).on('focus', '#roomful-url', function (event) {
+        event.preventDefault();
+        jQuery('#roomful-url').attr('placeholder', '');
+    }.bind(roomful));
+
+    jQuery(document.body).on('blur', '#roomful-url', function (event) {
+        event.preventDefault();
+        jQuery('#roomful-url').attr('placeholder', this.placeholder);
+    }.bind(roomful));
+
+    // Important inputs
+    jQuery(document.body).on('change', '#roomful-url', function (event) {
+        event.preventDefault();
+
+        try {
+            var needUpdate = false;
+
+            var value = jQuery('#roomful-url').value;
+
+            if (value.trim() === '') {
+                this.room = '55mx098565068t';
+                this.host = 'my.roomful.net';
+                this.update();
+
+                return true;
+            }
+
+            var url = (new URL(value));
+            if (url && url.hash) {
+                var hash = url.hash.trim();
+                var m = hash.match(/^#\/room\/(\w{14})?(?:[/?]|$)/);
+
+                if (m && m[1]) {
+                    this.room = m[1];
+                    needUpdate = true;
+                } else {
+                    this.room = '55mx098565068t';
+                    needUpdate = true;
+                }
+            }
+
+            if (url && url.host !== this.host) {
+                this.host = url.host;
+                needUpdate = true;
+            }
+
+            if (needUpdate) this.update();
+        } catch (e) {
+        }
+    }.bind(roomful));
+
+    jQuery(document.body).on('change', '#roomful-youtube', function (event) {
+        event.preventDefault();
+
+        var url = jQuery('#roomful-youtube').value;
+        try {
+            var needUpdate = false;
+            var m = url.match(/^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/);
+
+            if (m && m[1]) {
+                this.ytVideo = m[1];
+                needUpdate = true;
+            } else {
+                this.ytVideo = '';
+                needUpdate = true;
+            }
+
+            if (needUpdate) this.update();
+        } catch (e) {
+        }
+    }.bind(roomful));
+});
+
+
--- roomful.php
+++ roomful.php
@@ -20,24 +20,24 @@ function _get_plugin_url()
         : plugins_url(basename(dirname(__FILE__)));
 }
 
-function roomful_editor_scripts_enqueue($page)
+function roomful_editor_scripts_enqueue()
 {
-    if ($page !== 'post.php' && $page !== 'post-new.php' && (empty($_GET['page']))) {
-        return;
-    }
+    if (stripos($_SERVER['REQUEST_URI'], 'post.php') === FALSE
+        && stripos($_SERVER['REQUEST_URI'], 'post-new.php') === FALSE) return;
 
-    wp_register_script('roomful-js', _get_plugin_url() . '/js/editor.js', array('jquery'), '0.0.1');
-    wp_register_style('roomful-css', _get_plugin_url() . '/css/editor.css', array(), '0.0.1');
+    wp_register_script('roomful', _get_plugin_url() . '/js/editor.js', array('jquery'), '0.0.1');
+    wp_register_style('roomful', _get_plugin_url() . '/css/editor.css', array(), '0.0.1');
 
-    wp_enqueue_script('roomful-js');
-    wp_enqueue_style('roomful-css');
+    wp_enqueue_script('roomful');
+    wp_enqueue_style('awesome-font', 'https://use.fontawesome.com/releases/v5.1.1/css/all.css', array(), '5.1.1');
+    wp_enqueue_style('roomful');
 }
 
-function roomful_shortCode($attributes)
+function roomful_shortCode($attributes, $content = '')
 {
     $attributes = shortcode_atts(
         array(
-            'type' => 'frame',
+            'type' => 'iframe',
             'host' => 'my',
             'room' => '',
             'width' => 960,
@@ -45,6 +45,7 @@ function roomful_shortCode($attributes)
             'autoPlay' => 'false',
             'autoChat' => 'false',
             'needAuth' => 'false',
+            'isLight' => 'true',
             'ytVideo' => '',
         ), $attributes
     );
@@ -53,13 +54,33 @@ function roomful_shortCode($attributes)
     if ($attributes['autoPlay'] !== 'false') $query .= (($query === '') ? '?' : '&') . 'autoplay=1';
     if ($attributes['needAuth'] !== 'false') $query .= (($query === '') ? '?' : '&') . 'auth=1';
     if ($attributes['ytVideo'] !== '') $query .= (($query === '') ? '?' : '&') . 'v=' . esc_attr($attributes['ytVideo']);
-    if ($attributes['host'] !== 'my.roomful.net') $query .= (($query === '') ? '?' : '&') . 'light=1';
+    if ($attributes['host'] !== 'my' && $attributes['isLight'] === 'true') {
+        $query .= (($query === '') ? '?' : '&') . 'light=1';
+    } else if ($attributes['host'] === 'my' && $attributes['isLight'] === 'false') {
+        $query .= (($query === '') ? '?' : '&') . 'light=0';
+    }
 
-    $embedCode = '<iframe src="https://' . esc_attr($attributes['host']) . '.roomful.net/'
-        . ($query !== '' ? $query : '') . '#/room/' . esc_attr($attributes['room'])
-        . ($attributes['autoChat'] ? '/chat' : '')
-        . '" allow="autoplay; microphone; camera" frameborder="0"
+    $embedCode = '';
+    if ($attributes['type'] === 'iframe') {
+        $embedCode = '<iframe src="https://' . esc_attr($attributes['host']) . '.roomful.net/'
+            . ($query !== '' ? $query : '') . '#/room/' . esc_attr($attributes['room'])
+            . ($attributes['autoChat'] ? '/chat' : '')
+            . '" allow="autoplay; microphone; camera" frameborder="0"
          width="' . esc_attr($attributes['width']) . '" height="' . esc_attr($attributes['height']) . '"></iframe>';
+    } else if ($attributes['type'] === 'link' && $content === '') {
+        $embedCode = '<a href="https://' . esc_attr($attributes['host']) . '.roomful.net/'
+            . ($query !== '' ? $query : '') . '#/room/' . esc_attr($attributes['room'])
+            . ($attributes['autoChat'] ? '/chat' : '')
+            . '" target="_blank" title="Open Roomful">'
+            . '<img src="https://demo.roomful.co:3443/api/v0/resource/thumbnail/room/' .
+            esc_attr($attributes['room']) . '" width="' . esc_attr($attributes['width'])
+            . '" height="' . esc_attr($attributes['height']) . '"/></a>';
+    } else if ($attributes['type'] === 'link' && $content !== '') {
+        $embedCode = '<a href="https://' . esc_attr($attributes['host']) . '.roomful.net/'
+            . ($query !== '' ? $query : '') . '#/room/' . esc_attr($attributes['room'])
+            . ($attributes['autoChat'] ? '/chat' : '')
+            . '" target="_blank" title="Open Roomful">' . $content . '</a>';
+    }
 
     return $embedCode;
 }
@@ -70,30 +91,50 @@ function roomful_add_media_button()
         stripos($_SERVER['REQUEST_URI'], 'post-new.php') !== FALSE ||
         isset($_POST['action']) && $_POST['action'] == 'vc_edit_form'
     ) {
-        $tip = 'Insert a video';
+        $tip = 'Add Roomful';
         $icon = '<span></span>';
 
-        echo '<a title="' . __('Add Roomful', 'roomful-wordpress') . '" title="' . $tip . '" href="#" class="button froomful-wordpress-button" >' . $icon . ' Roomful</a>';
+        echo '<a title="' . __('Add Roomful', 'roomful-wordpress') . '" title="' . $tip
+            . '" href="#TB_inline?w=1&inlineId=roomful_insert_popup&width=780&height=470" class="thickbox button roomful-wordpress-button" >'
+            . $icon . ' Roomful</a>';
     }
 }
 
-//function roomful_media_menu($tabs)
-//{
-//    return array_merge($tabs, array('my_custom_tab' => __('Roomful')));
-//}
+function roomful_insert_popup()
+{
+    if (stripos($_SERVER['REQUEST_URI'], 'post.php') === FALSE
+        && stripos($_SERVER['REQUEST_URI'], 'post-new.php') === FALSE) return;
+
+    add_thickbox();
+
+    echo file_get_contents(__DIR__ . '/html/editor.html');
+}
 
 function wp_embed_handler_roomful($matches, $attributes, $url, $raw)
 {
-    $query = "https://" . esc_attr($matches[1]) . ".roomful.net" . ($matches[2] ? esc_attr(stripcslashes($matches[2])) : '');
-    $embed = '<iframe src="' . $query . '" allow="autoplay; microphone; camera" frameborder="0"  width="960" height="620"></iframe>';
+    $query = $matches[2] ? esc_attr(stripcslashes($matches[2])) : '';
+
+    //var_dump($query);
+
+    $href = "https://" . esc_attr($matches[1]) . ".roomful.net" . $query;
+    $embed = '<iframe src="' . $href
+        . '" allow="autoplay; microphone; camera" frameborder="0"  width="960" height="620"></iframe>';
     return apply_filters('embed_roomful', $embed, $matches, $attributes, $url, $raw);
 }
 
+/*
+function roomful_media_menu($tabs)
+{
+    return array_merge($tabs, array('my_custom_tab' => __('Roomful')));
+}
+
+add_filter('media_upload_tabs', 'roomful_media_menu');
+*/
+
 add_action('admin_enqueue_scripts', 'roomful_editor_scripts_enqueue');
+add_action('admin_footer', 'roomful_insert_popup', 100);
 add_action('media_buttons', 'roomful_add_media_button', 10);
-//add_filter('media_upload_tabs', 'roomful_media_menu');
 
 add_shortcode('roomful', 'roomful_shortCode');
-add_action('init', 'roomful_oembed_provider');
-wp_embed_register_handler('roomful', '#^https:\/\/(:my|beta|omega|zeta)\.roomful\.net(.+)$#i', 'wp_embed_handler_roomful');
+wp_embed_register_handler('roomful', '#^https:\/\/(:?my|beta|omega|zeta)\.roomful\.net(.+)$#i', 'wp_embed_handler_roomful');
 ?>
